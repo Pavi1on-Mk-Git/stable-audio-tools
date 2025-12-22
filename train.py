f49ad019b0426a8f41e49d57aa69a8a7b895a01a
Paweł Kochański <pawilioxion@gmail.com>
2025-12-22 19:29:37 +0100

    Reduced checkpoint size

--- train.py
+++ train.py
@@ -72,7 +72,11 @@ def main():
     checkpoint_dir = args.save_dir if args.save_dir else None
 
     ckpt_callback = pl.callbacks.ModelCheckpoint(
-        every_n_train_steps=args.checkpoint_every, dirpath=checkpoint_dir, save_top_k=-1
+        every_n_train_steps=args.checkpoint_every,
+        dirpath=checkpoint_dir,
+        save_top_k=-1,
+        save_weights_only=True,
+        filename="{step}",
     )
     save_model_config_callback = ModelConfigEmbedderCallback(model_config)
 
